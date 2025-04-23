71f966b05338fb9d1d4bb8ece576a8af5340dcf7
joaopedroalexan <joaopedroalexsilva@gmail.com>
2025-04-23 08:36:32 -0300

    ODeEventos

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import DefaltLayout from "./DefaltLayout";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({children}) =>{
-    const isAuthenticated = localStorage.getItem("authenticated");
-    return isAuthenticated ? <DefaltLayout>{children}</DefaltLayout> : <Navigate to="/"/>
-}
-export default ProtectedRoute
